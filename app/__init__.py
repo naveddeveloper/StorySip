fa6138080e7e31208fb4a526db8693b96136d448
Naved Ansari <ansarinavedhabeeb@gmaill.com>
2025-05-21 17:48:07 +0530

    fixed code

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 
-db = SQLAlchemy()
-login_manager = LoginManager()
-
-
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.DevelopmentConfig')  
+
+    db = SQLAlchemy(app)
+    login_manager = LoginManager()
 
     db.init_app(app)
     login_manager.init_app(app)
